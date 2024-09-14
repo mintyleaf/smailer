ea232b1ddcbb1225ae71da83f28a88b32ee8d44c
mintyleaf <mintyleafdev@gmail.com>
2024-09-14 06:46:23 +0400

    mailtrap api variant

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
-
-	"gopkg.in/mail.v2"
 )
 
 type Smailer struct {
@@ -19,6 +18,7 @@ type Smailer struct {
 	SMTPUser      string
 	SMTPPassword  string
 	TemplatesPath string
+	Token         string
 }
 
 type SendRequestBody struct {
@@ -27,6 +27,18 @@ type SendRequestBody struct {
 	Subject  string      `json:"subject"`
 	Template string      `json:"template"`
 	Values   interface{} `json:"values"`
+}
+
+type Address struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+type Mail struct {
+	From    Address   `json:"from"`
+	To      []Address `json:"to"`
+	Subject string    `json:"subject"`
+	HTML    string    `json:"html"`
 }
 
 func (s *Smailer) SendMail(w http.ResponseWriter, r *http.Request) {
@@ -52,18 +64,66 @@ func (s *Smailer) SendMail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	m := mail.NewMessage()
+	url := "https://send.api.mailtrap.io/api/send"
+	method := "POST"
+	mail := Mail{
+		From: Address{
+			Email: request.From,
+		},
+		To: []Address{
+			Address{
+				Email: request.To,
+			},
+		},
+		Subject: request.Subject,
+		HTML:    htmlBuffer.String(),
+	}
 
-	m.SetHeader("From", request.From)
-	m.SetHeader("To", request.To)
-	m.SetHeader("Subject", request.Subject)
-	m.SetBody("text/html", htmlBuffer.String())
-	d := mail.NewDialer(s.SMTPHost, s.SMTPPort, s.SMTPUser, s.SMTPPassword)
-	if err := d.DialAndSend(m); err != nil {
-		log.Printf("Failed to call SMTP relay: %v", err)
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	payload, err := json.Marshal(mail)
+	if err != nil {
+		log.Printf("Failed to marshal payload buffer %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, bytes.NewReader(payload))
+
+	if err != nil {
+		log.Printf("Failed to create mailtrap api request: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	req.Header.Add("Authorization", "Bearer "+s.Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("Failed to do mailtrap api request: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("Failed to read mailtrap response body: %s, %v", body, err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// m := mail.NewMessage()
+
+	// m.SetHeader("From", request.From)
+	// m.SetHeader("To", request.To)
+	// m.SetHeader("Subject", request.Subject)
+	// m.SetBody("text/html", htmlBuffer.String())
+	// d := mail.NewDialer(s.SMTPHost, s.SMTPPort, s.SMTPUser, s.SMTPPassword)
+	// if err := d.DialAndSend(m); err != nil {
+	// 	log.Printf("Failed to call SMTP relay: %v", err)
+	// 	http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	// 	return
+	// }
 }
 
 func lookupEnvPanic(key string) string {
@@ -94,6 +154,7 @@ func main() {
 		SMTPUser:      lookupEnvPanic("SMTP_USER"),
 		SMTPPassword:  lookupEnvPanic("SMTP_PASSWORD"),
 		TemplatesPath: templatesPath,
+		Token:         lookupEnvPanic("TOKEN"),
 	}
 
 	http.HandleFunc("/send", smailer.SendMail)
